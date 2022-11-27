98681900675c9b713e05eabcd3ab0df5217bb292
lappy <lappy@DESKTOP-DK7CVG9>
2022-11-27 12:06:25 +0530

    step 2

---
+++ BookStore/Controllers/BookController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        public string GetAllBooks()
+        {
+            return "showking all book";
+        }
+        public string GetOnlyOneBooks(int id)
+        {
+            return "Only book which id is"+ id;
+        }
+        public string SearchBooks(string author , string typeofbook)
+        {
+            return "showking all book in cataguory" + author + typeofbook;
+        }
+
+    }
+}

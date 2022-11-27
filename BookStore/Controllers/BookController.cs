9cc84906c2db359bb1d62db5a9d41de4791d3930
lappy <lappy@DESKTOP-DK7CVG9>
2022-11-27 14:52:13 +0530

    step3

--- BookStore/Controllers/BookController.cs
+++ BookStore/Controllers/BookController.cs
@@ -1,25 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookStore.Models;
 
 namespace BookStore.Controllers
 {
     public class BookController : Controller
     {
-        public string GetAllBooks()
+        public List<BookModel> listt = new List<BookModel>
         {
-            return "showking all book";
-        }
-        public string GetOnlyOneBooks(int id)
+            new BookModel { Id = 1, Author = "sourav", Title = "fun1" },
+
+            new BookModel { Id = 2, Author = "golu", Title = "fun2" } ,
+            new BookModel { Id = 3, Author = "muskan", Title = "fun3" },
+            new BookModel { Id = 4, Author = "shivam", Title = "fun4" }
+            
+
+        };
+        public List<BookModel> Index(int id)
         {
-            return "Only book which id is"+ id;
+            return listt;
         }
-        public string SearchBooks(string author , string typeofbook)
+        public List<BookModel> GetBooksById(int id)
         {
-            return "showking all book in cataguory" + author + typeofbook;
+            return listt.Where(x => x.Id == id).ToList();
         }
+        public List<BookModel> SearchBooks(string author,string title)
+        {
+            return listt.Where(x => x.Author.Contains(author) && x.Title.Contains(title)).ToList();
+        }
+
 
     }
 }
